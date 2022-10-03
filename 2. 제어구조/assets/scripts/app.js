e530a754e72bbc8b9923a1a8b07015530cd93b37
Takphantom <miroterra6@gmail.com>
2022-10-03 23:06:23 +0900

    2.제어구조 2.2 승패조건 설정

---
+++ 2. 제어구조/assets/scripts/app.js
@@ -0,0 +1,30 @@
+/** 플레이어의 데미지 최대값 */
+const ATTACK_VALUE = 10;
+
+/** 몬스터의 데미지 최대값 */
+const MONSTER_ATTACK_VALUE = 14;
+
+/** 기본 체력 */
+let chosenMaxLife = 100;
+
+let currentMonsterHealth = chosenMaxLife;
+let currentPlayerHealth = chosenMaxLife;
+
+adjustHealthBars(chosenMaxLife);
+
+function attackHandler() {
+  const damage = dealMonsterDamage(ATTACK_VALUE);
+  currentMonsterHealth -= damage;
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerHealth -= playerDamage;
+
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+    alert('You won!');
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    alert('You lost!');
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+    alert('You have a draw');
+  }
+}
+
+attackBtn.addEventListener('click', attackHandler);
