4eef354b60f2418b2604ade1fad677f72a257e02
Takphantom <miroterra@naver.com>
2022-10-04 22:05:40 +0900

    2. 제어구조 보너스생명 제어

--- 2. 제어구조/assets/scripts/app.js
+++ 2. 제어구조/assets/scripts/app.js
@@ -1,7 +1,11 @@
-/** 플레이어의 데미지 최대값 */
+/** 플레이어의 기본 데미지 최댓값 */
 const ATTACK_VALUE = 10;
+/** 플레이어의 강한 데미지 최댓값 */
+const STRONG_ATTACK_VALUE = 17;
+/** 플레이어의 회복 최댓값 */
+const HEAL_VALUE = 20;
 
-/** 몬스터의 데미지 최대값 */
+/** 몬스터의 데미지 최댓값 */
 const MONSTER_ATTACK_VALUE = 14;
 
 /** 기본 체력 */
@@ -9,16 +13,25 @@ let chosenMaxLife = 100;
 
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
+let hasBonusLife = true;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackHandler() {
-  const damage = dealMonsterDamage(ATTACK_VALUE);
-  currentMonsterHealth -= damage;
+/** 매 라운드마다 실행될 함수 */
+function endRound() {
+  const initialPlayerHealth = currentPlayerHealth;
   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
   currentPlayerHealth -= playerDamage;
 
+  if (currentPlayerHealth <= 0 && hasBonusLife) {
+    hasBonusLife = false;
+    removeBonusLife();
+    currentPlayerHealth = initialPlayerHealth;
+    setPlayerHealth(initialPlayerHealth);
+    alert('You would be dead but the bonus life saved you');
+  }
   if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+    // 게임의 승패조건
     alert('You won!');
   } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
     alert('You lost!');
@@ -27,4 +40,44 @@ function attackHandler() {
   }
 }
 
+/** 몬스터를 공격했을 때의 함수 */
+function attackMonster(mode) {
+  let maxDamage;
+  if (mode === 'ATTACK') {
+    maxDamage = ATTACK_VALUE;
+  } else if (mode === 'STORNG_ATTACK') {
+    maxDamage = STRONG_ATTACK_VALUE;
+  }
+
+  const damage = dealMonsterDamage(maxDamage);
+  currentMonsterHealth -= damage;
+  endRound();
+}
+
+/** Attack 버튼을 클릭했을 때의 함수 */
+function attackHandler() {
+  attackMonster('ATTACK');
+}
+
+/** Storng Attack 버튼을 클릭했을 때의 함수 */
+function strongAttackHandler() {
+  attackMonster('STRONG_ATTACK');
+}
+
+/** HEAL 버튼을 클릭했을 때의 함수 */
+function healPlayerHandler() {
+  let healValue;
+  if (currentPlayerHealth >= chosenMaxLife - HEAL_VALUE) {
+    alert("You can't heal to more than your max inital health. ");
+    healValue = chosenMaxLife - currentPlayerHealth;
+  } else {
+    healValue = HEAL_VALUE;
+  }
+  increasePlayerHealth(HEAL_VALUE);
+  currentPlayerHealth += HEAL_VALUE;
+  endRound();
+}
+
 attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
+healBtn.addEventListener('click', healPlayerHandler);
