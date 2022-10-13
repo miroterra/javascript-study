f5a6a363f1d8cdfc9d63594553ea47cce4c9de49
Takphantom <miroterra6@gmail.com>
2022-10-14 02:49:18 +0900

    3. 자바스크립트 var,let 차이

---
+++ 3. 자바스크립트/app.js
@@ -0,0 +1,20 @@
+// var, let 에 차이에 대해 알아봄 (대충)
+// let name = 'Max';
+
+// if (name === 'Max') {
+//   hobbies = ['Sports', 'Cooking'];
+//   console.log(hobbies);
+// }
+// function greet() {
+//   let age = 30;
+//   let name = 'Manuel';
+//   console.log(name, age, hobbies);
+// }
+
+// console.log(name, hobbies);
+
+// greet();
+
+var userName = 'Max';
+
+console.log(userName);
