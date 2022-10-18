5741db00cb77c974db517a3eb1276268d59ed20d
Takphantom <miroterra@naver.com>
2022-10-18 20:31:09 +0900

    4. 함수 1.함수,메서드

---
+++ 4. 함수/1. 함수 vs 메서드/app.js
@@ -0,0 +1,20 @@
+const startGameBtn = document.getElementById('start-game-btn');
+
+function startGame() {
+  console.log('Game is starting...');
+}
+
+// const person = {
+//   // name이 프로퍼티
+//   name: 'Max',
+
+//   // 객체에 함수가 저장된 것을 메서드 라고 부른다
+//   greet: function greet() {
+//     console.log('Hello there');
+//   },
+// };
+
+// person.greet();
+
+// 즉 addEventListener는 startGameBtn 객체의 메서드다
+startGameBtn.addEventListener('click', startGame);
