9710fbadea0df2baff7646c22b36f473879bf3af
Takphantom <miroterra@naver.com>
2022-10-24 23:46:57 +0900

    4. 함수 2-4 콜백함수

--- 4. 함수/2. 함수연습 게임만들기/app.js
+++ 4. 함수/2. 함수연습 게임만들기/app.js
@@ -81,23 +81,36 @@ startGameBtn.addEventListener('click', () => {
 
 // Rest 연산자
 
-const sumUp = (a, b, ...numbers) => {
+// 콜백함수
+const combine = (resultHandler, operation, ...numbers) => {
+  //함수 안에 함수
+  const validateNumber = number => {
+    return isNaN(number) ? 0 : number;
+  };
+
   let sum = 0;
   for (const num of numbers) {
-    sum += num;
+    if (operation === 'ADD') {
+      sum += validateNumber(num);
+    } else {
+      sum -= validateNumber(num);
+    }
+    resultHandler(sum);
   }
-  return sum;
 };
-
 // function 키워드를 쓸때만 사용 가능한 arguments는 배열과 비슷한 객체를 제공, 실제 배열은 아님
 // ES6에 Rest 연산자가 도입되기 이전에 사용, 현재는 Rest 연산자를 쓰자
-const subtractUp = function () {
-  let sum = 0;
-  for (const num of arguments) {
-    sum += num;
-  }
-  return sum;
+// const subtractUp = function (resultHandler, ...numbers) {
+//   let sum = 0;
+//   for (const num of numbers) {
+//     sum += num;
+//   }
+//   resultHandler(sum, 'The result after adding all numbers is');
+// };
+
+const showResult = result => {
+  alert('The result after adding all number is : ' + result);
 };
 
-console.log(sumUp(1, 10, -15, 20));
-console.log(subtractUp(10, 20, 15, -16, -20));
+combine(showResult.bind(this, 'The result after adding all numbers is :'), 'ADD', 1, 10, -15, 20);
+combine(showResult.bind(this, 'The result after subtracting all numbers is :'), 'SUBTRACT', 10, 20, 15, -16, -20);
