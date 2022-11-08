b37e1aafc953c01ae032253e748ac0eea06da5de
Takphantom <miroterra@naver.com>
2022-11-09 00:35:48 +0900

    7.객체 this

--- 7. 객체/1. 무비예제로 객체알아보기/assets/scripts/objects.js
+++ 7. 객체/1. 무비예제로 객체알아보기/assets/scripts/objects.js
@@ -14,17 +14,24 @@ const renderMovies = (filter = '') => {
   // 객체를 공부하는 단계라 간단하게 하기 이해 이 방법을 사용
   movieList.innerHTML = '';
 
-  const filteredMovies = !filter ? movies : movies.filter(movie => movie.info.title.includes(filter));
+  const filteredMovies = !filter ? movies : movies.filter((movie) => movie.info.title.includes(filter));
 
-  filteredMovies.forEach(movie => {
+  filteredMovies.forEach((movie) => {
     const movieEl = document.createElement('li');
     // 객체 구조 분해 - movie에 있는 info안에 있는 모든 프로퍼티를 빼냄
     // 따라서 기존에 movie.info로 접근을 했던걸 info 만으로도 접근 가능
     const { info, ...otherProps } = movie;
     console.log(otherProps);
     // const { title: movieTitle } = info;
-    // const { getFormattedTitle } = movie;
-    let text = movie.getFormattedTitle() + ' - ';
+    let { getFormattedTitle } = movie;
+
+    // this 가 올바른 값은 참조할 수 있도록 bind를 사용 - bind 는 나중에 실행할 함수를 준비하고 마지막에는 새로운 함수 객체를 반환
+    // getFormattedTitle = getFormattedTitle.bind(movie);
+
+    // bind와 다르게 call은 함수를 바로 실행
+    // apply는 call 과 마찬가지로 바로 실행하지만 첫번째 인자를 this로 받고 다음인자를 하나만 받을수 있다. 그 인자는 배열이어야한다.
+    let text = movie.getFormattedTitle.call(movie) + ' - ';
+    // let text = movie.getFormattedTitle.apply(movie, []) + ' - ';
     // 객체의 key,value 를 다루는 반복문
     for (const key in info) {
       if (key !== 'title') {
@@ -62,7 +69,8 @@ const addMovieHandler = () => {
   renderMovies();
 };
 
-const searchMovieHandler = () => {
+const searchMovieHandler = function () {
+  console.log(this);
   const filterTerm = document.getElementById('filter-title').value;
   renderMovies(filterTerm);
 };
