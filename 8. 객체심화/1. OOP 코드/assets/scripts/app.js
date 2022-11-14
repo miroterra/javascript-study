48a0e012cae0b5dedf98fbceaba8b5c1dd3dd830
Takphantom <miroterra@naver.com>
2022-11-14 23:31:47 +0900

    8.객체 심화 장바구니추가

--- 8. 객체심화/1. OOP 코드/assets/scripts/app.js
+++ 8. 객체심화/1. OOP 코드/assets/scripts/app.js
@@ -13,10 +13,29 @@ class Product {
   }
 }
 
+class ShoppingCart {
+  items = [];
+  render() {
+    const cartEl = document.createElement('section');
+    cartEl.innerHTML`
+    <h2>Total: \$${0}</h2>
+    <button>Order Now!</button>
+    `;
+    cartEl.className = 'cart';
+    return cartEl;
+  }
+}
+
 class ProductItem {
   constructor(product) {
     this.product = product;
   }
+
+  addToCart() {
+    console.log('Adding product to cart');
+    console.log(this.product);
+  }
+
   render() {
     const prodEl = document.createElement('li');
     prodEl.classname = 'product-item';
@@ -31,6 +50,9 @@ class ProductItem {
         </div>
       </div>
       `;
+
+    const addCartButton = prodEl.querySelector('button');
+    addCartButton.addEventListener('click', this.addToCart.bind(this));
     return prodEl;
   }
 }
@@ -42,17 +64,33 @@ class ProductList {
   ];
   constructor() {}
   render() {
-    const renderHook = document.getElementById('app');
     const prodList = document.createElement('ul');
     prodList.className = 'product-list';
     for (const prod of this.products) {
-      const productItem = new ProductItem();
+      const productItem = new ProductItem(prod);
       const prodEl = productItem.render();
       prodList.append(prodEl);
     }
-    renderHook.append();
+    return prodList;
   }
 }
+
+class Shop {
+  render() {
+    const renderHook = document.getElementById('app');
+
+    const cart = new ShoppingCart();
+    const cartEl = cart.render();
+    const productList = new ProductList();
+    productList.render();
+
+    renderHook.append(cart);
+    renderHook.append(prodListEl);
+  }
+}
+
+const shop = new Shop();
+shop.render();
 
 const productList = new ProductList();
 productList.render();
