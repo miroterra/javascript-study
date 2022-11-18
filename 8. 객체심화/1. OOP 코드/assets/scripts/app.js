33a5fd9cbaffd2c4933de4eac3b8cf725ef95136
Takphantom <miroterra@naver.com>
2022-11-18 23:54:42 +0900

    8.객체심화 상속1

--- 8. 객체심화/1. OOP 코드/assets/scripts/app.js
+++ 8. 객체심화/1. OOP 코드/assets/scripts/app.js
@@ -13,7 +13,35 @@ class Product {
   }
 }
 
-class ShoppingCart {
+class ElementAttribut {
+  constructor(attrName, attrValue) {
+    this.name = attrName;
+    this.value = attrValue;
+  }
+}
+
+// 상속 클래스
+class Component {
+  constructor(renderHookId) {
+    this.hookId = renderHookId;
+  }
+  createRootElement(tag, cssClasses, attributes) {
+    const rootElement = document.createElement(tag);
+    if (cssClasses) {
+      rootElement.className = cssClasses;
+    }
+    if (attributes && attributes.length > 0) {
+      for (const attr of attributes) {
+        rootElement.setAttribute(attr.name, attr.value);
+      }
+    }
+    document.getElementById(this.hookId).append(rootElement);
+    return rootElement;
+  }
+}
+
+// 상속
+class ShoppingCart extends Component {
   items = [];
 
   set cartItems(value) {
@@ -26,6 +54,10 @@ class ShoppingCart {
     return sum;
   }
 
+  constructor(renderHookId) {
+    super(renderHookId);
+  }
+
   addProuct(product) {
     const updatedItems = [...this.items];
     updatedItems.push(product);
@@ -33,14 +65,12 @@ class ShoppingCart {
   }
 
   render() {
-    const cartEl = document.createElement('section');
+    const cartEl = this.createRootElement('section', 'cart');
     cartEl.innerHTML`
     <h2>Total: \$${0}</h2>
     <button>Order Now!</button>
     `;
-    cartEl.className = 'cart';
     this.totalOutput = cartEl.querySelector('h2');
-    return cartEl;
   }
 }
 
@@ -96,8 +126,8 @@ class Shop {
   render() {
     const renderHook = document.getElementById('app');
 
-    this.cart = new ShoppingCart();
-    const cartEl = this.cart.render();
+    this.cart = new ShoppingCart('app');
+    this.cart.render();
     const productList = new ProductList();
     productList.render();
 
