75f83bcf030d579ffd3feeb4591d361da6a54e44
Takphantom <miroterra@naver.com>
2022-12-10 22:38:36 +0900

    8.객체심화 이벤트6

--- 8. 객체심화/3. APP연습/assets/scripts/events.js
+++ 8. 객체심화/3. APP연습/assets/scripts/events.js
@@ -4,7 +4,7 @@ const button = document.querySelector('button');
 
 // };
 
-const buttonClickHandler = (event) => {
+const buttonClickHandler = event => {
   // event.target.disabled = true;
   console.log(event);
 };
@@ -34,20 +34,35 @@ const boundFn = buttonClickHandler.bind(this);
 
 const form = document.querySelector('form');
 
-form.addEventListener('submit', (event) => {
+form.addEventListener('submit', event => {
   event.preventDefault();
   console.log(event);
 });
 
 const div = document.querySelector('div');
 
-div.addEventListener('mouseenter', (event) => {
+div.addEventListener('mouseenter', event => {
   console.log('CLICKED DIV');
   console.log(event);
 });
 
-button.addEventListener('mouseenter', (event) => {
+button.addEventListener('mouseenter', event => {
   event.stopPropagation();
   console.log('CLICKED BUTTON');
   console.log(event);
+});
+
+const listItems = document.querySelectorAll('li');
+const list = document.querySelector('ul');
+
+// listItems.forEach(listItem => {
+//   listItem.addEventListener('click', event => {
+//     event.target.classList.toggle('highlight');
+//   });
+// });
+
+list.addEventListener('click', event => {
+  // console.log(event.currentTarget);
+  // event.target.classList.toggle('highlight');
+  event.target.closest('li').classList.toggle('highlight');
 });
